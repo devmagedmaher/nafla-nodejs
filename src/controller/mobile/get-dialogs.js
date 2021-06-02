e98cc668029e9c12cbab3e39793f6051b34226c0
Maged Maher <dev.magedmaher@gmail.com>
2021-06-02 20:38:10 +0200

    filter by metadata.visible

--- src/controller/mobile/get-dialogs.js
+++ src/controller/mobile/get-dialogs.js
@@ -4,18 +4,11 @@ const { assistantV1 } = require('../../services/ibm-watson');
 module.exports = async (req, res, next) => {
 
   const data = await assistantV1.listDialogNodes({
-    workspaceId: process.env.WATSON_ASSISTANT_SKILL_ID,
-    // workspaceId: '1568ff62-a9bf-4da2-a8a2-bd0bc442353f',
-    // workspaceId: 'b90775b4-8294-40d7-9782-41f0cc69b089',
+    workspaceId: 'b90775b4-8294-40d7-9782-41f0cc69b089',
   }).then(response => response.result.dialog_nodes)
 
   // filter dialog nodes with common conditions
-    .then(nodes => nodes.filter(node => {
-      if (node.conditions === 'welcome' || node.conditions === 'anything_else') {
-        return false
-      }
-      return true;
-    }))
+    .then(nodes => nodes.filter(node => node.metadata && node.metadata.visible))
   
   // map dialog nodes to { id, text } object
     .then(nodes => nodes.map(node => ({
